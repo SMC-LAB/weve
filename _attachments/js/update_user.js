4c280bbe488997af0dfe3638ba3396ef5ca3224f
Pedro Silva <pedro.alex.silva@gmail.com>
2011-10-04 16:57:50 +0100

    Fixed problem around jquery.js versions in ubuntu's shipped couchdb
    * ubuntu ships jquery.js 1.4.2
    * tested on 1.6.2
    * update_user() can't use [eventData] in submit() in 1.4.2 (>1.4.3)

--- _attachments/js/update_user.js
+++ _attachments/js/update_user.js
@@ -1,4 +1,5 @@
-function update_user(options) {
+function update_user() {
+v    var that = this;
     var form_data = $(this).serializeArray();
     $.couch.session({
         success:function(d) {
@@ -8,6 +9,7 @@ function update_user(options) {
             var id = 'org.couchdb.user:' + d.userCtx.name;
             console.log("update user: " + id);
             $.getJSON("/_users/" + id, function(data) {
+                console.log($(that).attr('action'));
                 for (var i = 0; i < form_data.length; i++) {
                     data[form_data[i].name] = form_data[i].value;
                 }
@@ -16,7 +18,7 @@ function update_user(options) {
                     url:"/_users/" + id,
                     dataType: 'json',
                     data: JSON.stringify(data),
-                    success:function(d) {window.location.replace(options.data.redirect);}
+                    success:function(d) {window.location.replace($(that).attr('action'));}
                 });
             });
         }
