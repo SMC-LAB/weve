73256042ace74e27be33f39613e3a0eba2de09f3
Pedro Silva <psilva+git@pedrosilva.pt>
2013-04-09 15:31:59 +0100

    Add pagination information

--- _attachments/js/app.js
+++ _attachments/js/app.js
@@ -1,134 +1,151 @@
-function redirect(url) {
-  if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){
-    var referLink = document.createElement('a');
-    referLink.href = url;
-    document.body.appendChild(referLink);
-    referLink.click();
-  } else {
-    location.href = url;
-  }
+   function redirect(url) {
+    if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)){
+	var referLink = document.createElement('a');
+	referLink.href = url;
+	document.body.appendChild(referLink);
+	referLink.click();
+    } else {
+	location.href = url;
+    }
 }
 function init_weve(docs) {
-  return {
-    next:function(){docs.rows.shift(); if (docs.rows[0]) {return docs.rows[0].id;} else {return null}},
-    current:function(){return docs.rows[0].id;}
-  };
+    var total_pages = docs.rows.length;
+    return {
+	current_page: 1,
+	next:function(){
+	    this.current_page += 1;
+	    docs.rows.shift();
+	    if (docs.rows[0]) {
+		return docs.rows[0].id;
+	    }
+	    else {
+		return null;
+	    }
+	},
+	current:function(){return docs.rows[0].id;},
+	length:function(){return total_pages;},
+    };
 }
 function load_form(id) {
-  console.log("load_form(): " + id);
-  $('div#tags').load("_show/range/" + id);
+    //console.log("load_form(): " + id);
+    $('div#tags').load("_show/range/" + id);
 }
 function load_player(id) {
-  console.log("load_player(): " + id);
-  $('div#player').load("_show/player/" + id);    
+    //console.log("load_player(): " + id);
+    $('div#player').load("_show/player/" + id);    
 }
 
 function check_user() {
-  $.couch.session({
-    success:function(d) {
-      if (d.userCtx && d.userCtx.name) {
-        return false;
-      }
-      window.location.replace('/simple-melody/_design/weve/welcome.html');
-      return false;
-    }
-  })
+    $.couch.session({
+	success:function(d) {
+	    if (d.userCtx && d.userCtx.name) {
+		return false;
+	    }
+	    window.location.replace('/experiment-c/_design/weve/welcome.html');
+	    return false;
+	}
+    })
 }
 
 function submit_form() {
-  check_user();
-  if (!ids.current) {
-    return false;
-  }
-  var form = $('#save-ratings-form');
-  var data = form.serializeArray();
-  var id   = ids.current();
-  var action = "_update/save-ratings/" + id;
+    check_user();
+    if (!ids.current) {
+	return false;
+    }
 
-  form.submit(function() {
-    console.log("submit_form(): " + id);
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: data,
-      retries: 0,
-      maxretries: 10,
-      success: function() {
-        //console.log("No conflicts");
-      },
-      error: function(req, status, error) {
-        if (req.status === 409 || status === 'timeout') {
-          if (this.retries <= this.maxretries) {
-            $.ajax(this);
-          }
-          else {
-            console.log("Couldn't update document due to conflicts after " + this.retries);
-            return false;
-          }
-        }
-      }
+    var form = $('#save-ratings-form');
+    var data = form.serializeArray();
+    var id   = ids.current();
+    var action = "_update/save-ratings/" + id;
+
+    form.submit(function() {
+	//console.log("submit_form(): " + id);
+	$.ajax({
+	    type: "POST",
+	    url: action,
+	    data: data,
+	    retries: 0,
+	    maxretries: 10,
+	    success: function() {
+		//console.log("No conflicts");
+	    },
+	    error: function(req, status, error) {
+		if (req.status === 409 || status === 'timeout') {
+		    if (this.retries <= this.maxretries) {
+			$.ajax(this);
+		    }
+		    else {
+			//console.log("Couldn't update document due to conflicts after " + this.retries);
+			return false;
+		    }
+		}
+	    }
+	});
     });
-  });
 
-  id = ids.next();
-  if (!id) {
-    redirect("finish.html");
-  }
-  load_form(id);
-  load_player(id);
+    id = ids.next();
+    $("#pages").text(ids.current_page + '/' + ids.length());
 
-  return false;
+    if (!id) {
+	redirect("finish.html");
+    }
+    load_form(id);
+    load_player(id);
+
+    return false;
 }
 
 function validate_submission() {
-  var form = $('#save-ratings-form');
-  var labels = form.find('label');
-  var valid = true;
-  for (var i = 0; i < labels.length; i++) {
-    console.log(i + ": checking " + labels.eq(i).text());
-    if (labels.eq(i).text() === 'Not yet rated' ||
-        labels.eq(i).text() === 'Please rate this item') {
-      labels.eq(i).text("Please rate this item");
-      labels.eq(i).css('color', 'red');
-      valid = false;
+    var form = $('#save-ratings-form');
+    var labels = form.find('label');
+    var valid = true;
+    for (var i = 0; i < labels.length; i++) {
+	//console.log(i + ": checking " + labels.eq(i).text());
+	if (labels.eq(i).text() === 'Not yet rated' ||
+            labels.eq(i).text() === 'Please rate this item') {
+	    labels.eq(i).text("Please rate this item");
+	    labels.eq(i).css('color', 'red');
+	    valid = false;
+	}
     }
-  }
-  return valid;
+    return valid;
 }
 function player_playing() {
-  console.log("playing");
-  var form = $('#save-ratings-form');
-  form.find('.likert-item').attr('disabled', 'disabled');
-  form.find('#submit-button').attr('disabled', 'disabled');
-  form.find('#submit-button').attr('value', 'Wait');
+    //console.log("playing");
+    var form = $('#save-ratings-form');
+    form.find('.likert-item').attr('disabled', 'disabled');
+    form.find('#submit-button').attr('disabled', 'disabled');
+    form.find('#submit-button').attr('value', 'Wait');
 }
 function player_paused() {
-  console.log("paused");
-  var form = $('#save-ratings-form');
-  form.find('.likert-item').removeAttr('disabled');
-  form.find('#submit-button').removeAttr('disabled');
-  form.find('#submit-button').attr('value', 'Next');
+    //console.log("paused");
+    var form = $('#save-ratings-form');
+    form.find('.likert-item').removeAttr('disabled');
+    form.find('#submit-button').removeAttr('disabled');
+    form.find('#submit-button').attr('value', 'Next');
 }
 
 var ids;
 function start_weve() {
-  check_user();
-  $(document).ready(function() {
-    var dbname = document.location.href.split('/')[3];
-    var view   = 'random' // new Date().getTime() % 2 == 0 ? 'even' : 'odd';
+    check_user();
+    window.onbeforeunload = function(){return "Are you sure?";};
+    $(document).ready(function() {
+	var dbname = document.location.href.split('/')[3];
+	var view   = 'random' // new Date().getTime() % 2 == 0 ? 'even' : 'odd';
 
-    $db = $.couch.db(dbname);
-    $db.view("weve/" + view, {
-      success: function(data) {
-        ids = init_weve(data);
-        var id = ids.current();
-        load_form(id);
-        load_player(id);
-      },
-      error: function(status) {
-        console.log(status);
-      },
-      reduce: false
+	$db = $.couch.db(dbname);
+	$db.view("weve/" + view, {
+	    success: function(data) {
+		ids = init_weve(data);
+		var id = ids.current();
+		$("#pages").append(ids.current_page + '/' + ids.length());
+		load_form(id);
+		load_player(id);
+	    },
+	    error: function(status) {
+		//console.log(status);
+	    },
+	    reduce: false
+	});
     });
-  });
 }
